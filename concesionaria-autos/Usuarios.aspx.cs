bd96c66bae453d68dfc7bffea046b17f6a58f406
Charly <charly91.3ds@gmail.com>
2023-06-19 18:15:05 -0300

    iniciando lista de usuarios

---
+++ concesionaria-autos/Usuarios.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using dominio;
+using negocio;
+
+namespace concesionaria_autos
+{
+    public partial class Usuarios : System.Web.UI.Page
+    {
+        public bool FiltroAvanzado { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Seguridad.esAdmin(Session["trainee"]))
+            {
+                Session.Add("error", "Se requiere permisos de admin para acceder a esta pantalla");
+                Response.Redirect("Error.aspx");
+            }
+
+
+            FiltroAvanzado = chkAvanzado.Checked;
+            if (!IsPostBack)
+            {
+                UsuarioNegocio negocio = new UsuarioNegocio();
+                Session.Add("listaUsuarios", negocio.listarConSP());
+                dgvUsuarios.DataSource = Session["listaUsuarios"];
+                dgvUsuarios.DataBind();
+            }
+        }
+
+        protected void dgvUsuarios_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            dgvUsuarios.PageIndex = e.NewPageIndex;
+            dgvUsuarios.DataBind();
+        }
+
+        protected void dgvUsuarios_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string id = dgvUsuarios.SelectedDataKey.Value.ToString();
+            Response.Redirect("FormularioUsuario.aspx?id=" + id);
+        }
+
+        protected void filtro_TextChanged(object sender, EventArgs e)
+        {
+            List<Usuario> lista = (List<Usuario>)Session["listaUsuarios"];
+            List<Usuario> listaFiltrada = lista.FindAll(x => x.Nombre.ToUpper().Contains(txtFiltro.Text.ToUpper()));
+            dgvUsuarios.DataSource = listaFiltrada;
+            dgvUsuarios.DataBind();
+        }
+
+        protected void chkAvanzado_CheckedChanged(object sender, EventArgs e)
+        {
+            FiltroAvanzado = chkAvanzado.Checked;
+            txtFiltro.Enabled = !FiltroAvanzado;
+        }
+
+        protected void ddlCampo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ddlCriterio.Items.Clear();
+            if (ddlCampo.SelectedItem.ToString() == "Número")
+            {
+                ddlCriterio.Items.Add("Igual a");
+                ddlCriterio.Items.Add("Mayor a");
+                ddlCriterio.Items.Add("Menor a");
+            }
+            else
+            {
+                ddlCriterio.Items.Add("Contiene");
+                ddlCriterio.Items.Add("Comienza con");
+                ddlCriterio.Items.Add("Termina con");
+            }
+        }
+
+        protected void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                UsuarioNegocio negocio = new UsuarioNegocio();
+                dgvUsuarios.DataSource = negocio.filtrar(
+                    ddlCampo.SelectedItem.ToString(),
+                    ddlCriterio.SelectedItem.ToString(),
+                    txtFiltroAvanzado.Text,
+                    ddlEstado.SelectedItem.ToString());
+                dgvUsuarios.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex);
+                throw;
+            }
+        }
+    }
+}
--- concesionaria-autos/detalle.aspx
+++ concesionaria-autos/detalle.aspx
@@ -32,12 +32,21 @@
     <div class="equipamiento">
         <div class="foto">
             <img src="https://cdn.group.renault.com/ren/ar/modelos/kangoo/ph2/renault-kangoo-k61-ph2-interior-equipamiento-002.jpg.ximg.large.webp/ddaf4a96b7.webp" alt="Alternate Text" style="width:400px;" />
+            <h4>
+                <span class="price">Interior</span>
+            </h4>
         </div>
         <div class="foto">
             <img src="https://cdn.group.renault.com/ren/ar/modelos/kangoo/ph2/kangoo-k61-ph2-interior-equipamiento-07.jpg.ximg.large.webp/5b19d46677.webp" alt="Alternate Text" style="width:400px;" />
+            <h4>
+                <span class="price">Interior</span>
+            </h4>
         </div>
         <div class="foto">
             <img src="https://cdn.group.renault.com/ren/ar/modelos/kangoo/ph2/kangoo-k61-ph2-interior-equipamiento-08.jpg.ximg.xsmall.jpg/cdacd8fcf5.jpg" alt="Seguridad activa" style="width:400px;"/>
+            <h4>
+                <span class="price">Interior</span>
+            </h4>
         </div>
     </div>
 
