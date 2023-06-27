028fcf7401d5da2c816a154245d66d668bf2402d
Maria Clara Visconti <claravisconti@hotmail.com>
2023-06-27 17:52:28 -0300

    clases y negocio concesionaria

---
+++ dominio/Concesionaria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Concesionaria
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string ImagenUrl { get; set; }
+        public string MapsUrl { get; set; }
+        public int SemanaAbre { get; set; }
+        public int SemanaCierra { get; set; }
+        public int SabadoAbre { get; set; }
+        public int SabadoCierra { get; set; }
+        public string Localidad { get; set; }
+        public string Provincia { get; set; }
+    }
+}
