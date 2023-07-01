4ac7ce72e21235d1ce719c55efa87cc53769f592
Charly <charly91.3ds@gmail.com>
2023-07-01 16:58:40 -0300

    a

--- negocio/EquipamientoNegocio.cs
+++ negocio/EquipamientoNegocio.cs
@@ -55,5 +55,50 @@ namespace negocio
                 datos.cerrarConexion();
             }
         }
+
+        public List<Equipamiento> listarUnModelo(int idModelo)
+        {
+            List<Equipamiento> lista = new List<Equipamiento>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("select id,Nombre,Precio,Climatizador,SensorEstacionamiento,Computadora,ControlCrucero,Alarma,IdProducto from equipamiento where IdProducto = " + idModelo);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Equipamiento aux = new Equipamiento();
+
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Precio = (decimal)datos.Lector["Precio"];
+                    aux.Climatizador = (bool)datos.Lector["Climatizador"];
+                    aux.SensorEstacionamiento = (bool)datos.Lector["SensorEstacionamiento"];
+                    aux.Computadora = (bool)datos.Lector["Computadora"];
+                    aux.ControlCrucero = (bool)datos.Lector["ControlCrucero"];
+                    aux.Alarma = (bool)datos.Lector["Alarma"];
+                    aux.IdProducto = (int)datos.Lector["IdProducto"];
+                    if (!(datos.Lector["ImagenUrl"] is DBNull))
+                    {
+                        aux.Imagen = new Imagen();
+                        aux.Imagen.ImagenUrl = (string)datos.Lector["ImagenUrl"];
+                    }
+                    lista.Add(aux);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+
     }
 }
